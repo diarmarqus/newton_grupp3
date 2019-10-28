c900a827e34c2d5032de6fbeb132e29accaba5d6
Max Cornett <max.cornett@gmail.com>
2019-10-28 15:28:47 +0100

    test

--- PizzaPalatset/PizzaPalatset/Program.cs
+++ PizzaPalatset/PizzaPalatset/Program.cs
@@ -6,7 +6,7 @@ namespace PizzaPalatset
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello W");
         }
     }
 }
