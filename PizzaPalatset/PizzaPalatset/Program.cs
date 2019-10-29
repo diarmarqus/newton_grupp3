e051754d9c2b17508a6c308031712286db31c1f9
ACADEMY-6303P31\Administrator <diar.marqus@gmail.com>
2019-10-29 15:13:20 +0100

    diar testar

--- PizzaPalatset/PizzaPalatset/Program.cs
+++ PizzaPalatset/PizzaPalatset/Program.cs
@@ -7,6 +7,7 @@ namespace PizzaPalatset
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Diar Testar");
         }
     }
 }
