43ba3a7917fa9762b009731a67fdcaa1957c9ec5
mol1987 <57093339+mol1987@users.noreply.github.com>
2019-11-05 15:48:27 +0100

    alala

---
+++ PizzaWorld/PizzaWorld/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace PizzaWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Diar Testar");
+            Console.WriteLine("hello from molle");
+            Console.WriteLine("Hej från Aata");
+        }
+    }
+}
