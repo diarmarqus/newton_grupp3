8598192bde815478dc33153f1403cc0c1f5c9cb9
Max Cornett <max.cornett@gmail.com>
2019-11-07 11:29:55 +0100

    max

---
+++ PizzaWorld/PizzaBagare/Terminal.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PizzaBagare
+{
+    class Terminal
+    {
+        public List<Order> Orders { get; private set; }
+        public Chef Chef { get; private set; }
+
+        public void LogIn(Data data, Display display)
+        {
+            Chef chef = null;
+            display.PrintLogInInfo();
+            while (chef == null)
+            {
+                Console.Write("Pin: ");
+                bool isNum = int.TryParse(Console.ReadLine(), out int pin);
+
+                if (!isNum)
+                {
+                    display.PrintLogInInfo("Endast siffror");
+                    continue;
+                }
+
+                chef = data.Chefs.FirstOrDefault(c => c.Pin == pin);
+
+                if (chef == null)
+                {
+                    display.PrintLogInInfo("Fel kod");
+                }
+            }
+
+            SetData(data, chef);
+        }
+
+        private void SetData(Data data, Chef chef)
+        {
+            this.Chef = chef;
+            this.Orders = data.Orders;
+        }
+
+        //private Chef VerifyPin(Data data, int pin) => data.Chefs.FirstOrDefault(c => c.Pin == pin);
+
+        public void GetOrders(Display display) => display.PrintOrders(Orders);
+        public void GetOrderDetails(Display display, int index)
+        {
+            try
+            {
+                display.PrintOrderDetails(Orders[index - 1]);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+    }
+}
---
+++ PizzaWorld/PizzaWorld/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace PizzaWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Diar Testar");
+            Console.WriteLine("hello from molle");
+            Console.WriteLine("Hej från Aata");
+            Console.WriteLine("Hej Aata här");
+            Console.WriteLine("asdsd");
+        }
+    }
+}
