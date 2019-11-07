ed30286f62d780661b2202b05fe50caac0c7ef55
Max Cornett <max.cornett@gmail.com>
2019-11-07 11:34:43 +0100

    -

---
+++ PizzaWorld/PizzaBagare/Data.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PizzaBagare
+{
+    class Data
+    {
+        public List<Order> Orders = new List<Order>();
+        public List<Chef> Chefs = new List<Chef>();
+
+        public Data()
+        {
+            AddData();
+        }
+
+        /// <summary>
+        /// Genera data för Orders/Chefs
+        /// </summary>
+        public void AddData()
+        {
+            Orders.Add(new Order(
+                4404,
+                new List<Pizza>() {
+                    new Pizza("Vesuvio"),
+                    new Pizza("Kebab")
+                },
+                new List<Extra>() {
+                    new Extra("Cola", "Medium"),
+                    new Extra("Sallad")
+                }));
+
+            Orders.Add(new Order(
+                1110,
+                new List<Pizza>() {
+                    new Pizza("Egen", new List<string> { "Ost", "Skinka", "Tomater" }, "Standard", "Inbakad"),
+                    new Pizza("Kebab")
+                },
+                new List<Extra>() {
+                    new Extra("Pepsi", "Stor"),
+                    new Extra("Cola", "Stor"),
+                    new Extra("Sallad")
+                }));
+
+            Orders.Add(new Order(
+                2022,
+                new List<Pizza>() {
+                    new Pizza("Vesuvio"),
+                    new Pizza("Kebab")
+                },
+                new List<Extra>() {
+                    new Extra("Cola", "Medium"),
+                    new Extra("Sallad")
+                }));
+
+            Chefs.Add(new Chef(1111, "Pizzabagare 1"));
+            Chefs.Add(new Chef(2222, "Pizzabagare 2"));
+            Chefs.Add(new Chef(3333, "Pizzabagare 3"));
+        }
+    }
+}
